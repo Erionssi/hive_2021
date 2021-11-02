240a96fd8e03aab6c4d2e55b46e18a0615d215de
Jere Turunen <jturunen@student.hive.fi>
2021-11-02 10:29:10 +0200

    ex23 done, norm checked

---
+++ ex23/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jturunen <jturunen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 10:15:58 by jturunen          #+#    #+#             */
+/*   Updated: 2021/11/02 10:21:52 by jturunen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	set_point(t_point *point);
+
+#endif
